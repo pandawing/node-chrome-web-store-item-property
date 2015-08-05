74e3e5701b0298768d5e52f5f195d11cc3abf111
sanemat <o.gata.ken@gmail.com>
2015-08-06 06:08:57 +0900

    chore(test): add fetch test

--- test.js
+++ test.js
@@ -1,10 +1,23 @@
 'use strict';
 var assert = require('power-assert');
 var chromeWebstoreItemProperty = require('./');
+var nock = require('nock');
+var path = require('path');
+var shouldFulfilled = require('promise-test-helper').shouldFulfilled;
 
-it('should ', function () {
-  assert.strictEqual(chromeWebstoreItemProperty('unicorns'), 'unicorns & rainbows');
-});
-it('should not ', function () {
-  assert.strictEqual(chromeWebstoreItemProperty('unicorns'), 'unicorns & wrong');
+describe('#fetch', function () {
+  var identifier = 'nimelepbpejjlbmoobocpfnjhihnpked';
+  context('resource exists', function () {
+    it('should return fetched data', function () {
+      nock('https://chrome.google.com')
+        .get('/webstore/detail/' + identifier)
+        .replyWithFile(200, path.join(__dirname, identifier));
+      return shouldFulfilled(
+        chromeWebstoreItemProperty
+          .fetch(identifier)
+      ).then(function (value) {
+          assert(value);
+        });
+    });
+  });
 });
