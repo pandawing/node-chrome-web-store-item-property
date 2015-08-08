c9a1662dacb9bd49abb4040c7393b25f2b4c3713
sanemat <o.gata.ken@gmail.com>
2015-08-08 12:43:55 +0900

    chore(merge-config): add guard logic

--- src/merge-config.js
+++ src/merge-config.js
@@ -2,14 +2,21 @@ var objectAssign = require('object-assign');
 
 module.exports = function (url, defaultConfig, userConfig) {
   userConfig = userConfig || {};
+  defaultConfig = defaultConfig || {};
   var opts = objectAssign({}, defaultConfig, userConfig);
   opts.url = url;
-  var headerCandidate = [{}, defaultConfig.headers];
+  var headerCandidate = [{}];
+  if (defaultConfig.hasOwnProperty('headers')) {
+    headerCandidate.push(defaultConfig.headers);
+  }
   if (userConfig.hasOwnProperty('headers')) {
     headerCandidate.push(userConfig.headers);
   }
   opts.headers = objectAssign.apply(null, headerCandidate);
-  var qsCandidate = [{}, defaultConfig.qs];
+  var qsCandidate = [{}];
+  if (defaultConfig.hasOwnProperty('qs')) {
+    qsCandidate.push(defaultConfig.qs);
+  }
   if (userConfig.hasOwnProperty('qs')) {
     qsCandidate.push(userConfig.qs);
   }
