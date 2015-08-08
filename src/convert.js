3d9aa67751601352e502f58e51bfecb3b381bde8
sanemat <o.gata.ken@gmail.com>
2015-08-09 00:28:31 +0900

    feat(convert): ratingCount, ratingValue, UserDownloads to float
    fixes #14
    
    Before
    {
      "name": "Gmail Offline",
      "url": "https://chrome.google.com/webstore/detail/gmail-offline/ejidjjhkpiempkbhmpbfngldlkglhimk",
      "image": "https://lh4.googleusercontent.com/RpEbYErNLKvSWgk6I08RCwYdaO8ss2XObyvI9tibrvKmMAIIvV2rxFStRIZYwj0vI91TTv3MMA=s128-h128-e365",
      "version": "1.20",
      "price": "$0",
      "priceCurrency": "USD",
      "interactionCount": {
        "UserDownloads": "4,410,977"
      },
      "operatingSystems": "Chrome",
      "ratingValue": "4.137782288759198",
      "ratingCount": "11823",
      "id": "ejidjjhkpiempkbhmpbfngldlkglhimk"
    }
    
    After
    {
      "name": "Gmail Offline",
      "url": "https://chrome.google.com/webstore/detail/gmail-offline/ejidjjhkpiempkbhmpbfngldlkglhimk",
      "image": "https://lh4.googleusercontent.com/RpEbYErNLKvSWgk6I08RCwYdaO8ss2XObyvI9tibrvKmMAIIvV2rxFStRIZYwj0vI91TTv3MMA=s128-h128-e365",
      "version": "1.20",
      "price": "$0",
      "priceCurrency": "USD",
      "interactionCount": {
        "UserDownloads": 4410977
      },
      "operatingSystems": "Chrome",
      "ratingValue": 4.137782288759198,
      "ratingCount": 11823,
      "id": "ejidjjhkpiempkbhmpbfngldlkglhimk"
    }

--- src/convert.js
+++ src/convert.js
@@ -2,20 +2,38 @@
 var InvalidFormatError = require('./error').InvalidFormatError;
 var Promise = Promise || require('es6-promise').Promise;
 var cheerio = require('cheerio');
+var includes = require('array-includes');
+var parseFloatWithComma = require('./parse-float-with-comma');
+var keysStringToFloat = [
+  'ratingCount',
+  'ratingValue',
+  'UserDownloads'
+];
 
 function convert(detailHtml) {
   return new Promise(function (resolve, reject) {
     var $ = cheerio.load(detailHtml);
     var itemProps = {};
+    var elementItemProp;
     $('meta[itemprop]').each(function (index, element) {
       // Split content like <meta itemprop="interactionCount" content="UserDownloads:418" />
       if ($(element).attr('itemprop') === 'interactionCount' &&
         $(element).attr('content').indexOf(':') !== -1) {
         var keyValue = $(element).attr('content').split(':', 2);
-        itemProps[$(element).attr('itemprop')] = itemProps[$(element).attr('itemprop')] || {};
-        itemProps[$(element).attr('itemprop')][keyValue[0]] = keyValue[1];
+        elementItemProp = $(element).attr('itemprop');
+        itemProps[elementItemProp] = itemProps[elementItemProp] || {};
+        if (includes(keysStringToFloat, keyValue[0])) {
+          itemProps[elementItemProp][keyValue[0]] = parseFloatWithComma(keyValue[1]);
+        } else {
+          itemProps[elementItemProp][keyValue[0]] = keyValue[1];
+        }
       } else {
-        itemProps[$(element).attr('itemprop')] = $(element).attr('content');
+        elementItemProp = $(element).attr('itemprop');
+        if (includes(keysStringToFloat, elementItemProp)) {
+          itemProps[elementItemProp] = parseFloatWithComma($(element).attr('content'));
+        } else {
+          itemProps[elementItemProp] = $(element).attr('content');
+        }
       }
     });
     if (Object.keys(itemProps).length === 0) {
