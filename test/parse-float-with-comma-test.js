5b8b9ebe5be33db4905a5d6d729f8bcbd2d766a6
sanemat <o.gata.ken@gmail.com>
2015-08-09 00:55:45 +0900

    chore(parse-float-with-comma): fix test description

--- test/parse-float-with-comma-test.js
+++ test/parse-float-with-comma-test.js
@@ -4,30 +4,30 @@ var parseFloatWithComma = require('../src/parse-float-with-comma');
 
 describe('parse-float-with-comma', function () {
   context('with comma', function () {
-    it('"," is NaN', function () {
+    it('"," to NaN', function () {
       assert(isNaN(parseFloatWithComma(',')));
     });
-    it('1,004 equals 1004', function () {
+    it('"1,004" to 1004', function () {
       assert.strictEqual(parseFloatWithComma('1,004'), 1004);
     });
-    it('1,000,004 equals 1000004', function () {
+    it('"1,000,004" to 1000004', function () {
       assert.strictEqual(parseFloatWithComma('1,000,004'), 1000004);
     });
-    it('1,000,004.1 equals 1000004.1', function () {
+    it('"1,000,004.1" to 1000004.1', function () {
       assert.strictEqual(parseFloatWithComma('1,000,004.1'), 1000004.1);
     });
   });
   context('without comma', function () {
-    it('"" is NaN', function () {
+    it('"" to NaN', function () {
       assert(isNaN(parseFloatWithComma('')));
     });
-    it('1004 is 1004', function () {
+    it('"1004" to 1004', function () {
       assert.strictEqual(parseFloatWithComma('1004'), 1004);
     });
-    it('1,000,004 is 1000004', function () {
+    it('"1000004" to 1000004', function () {
       assert.strictEqual(parseFloatWithComma('1000004'), 1000004);
     });
-    it('1000004.1 is 1000004.1', function () {
+    it('"1000004.1" to 1000004.1', function () {
       assert.strictEqual(parseFloatWithComma('1000004.1'), 1000004.1);
     });
   });
