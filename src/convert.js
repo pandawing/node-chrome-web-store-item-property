13eaa9d6bcb638c08373418bc58a27e558cb1836
Matt (Sanemat) <o.gata.ken@gmail.com>
2019-11-08 23:28:39 +0700

    refactor(convert): get itemprop value once

--- src/convert.js
+++ src/convert.js
@@ -16,31 +16,29 @@ var keysStringToFloat = [
 function convert(detailHtml) {
   return new Promise(function (resolve, reject) {
     var itemProps = {};
-    var elementItemProp;
     var options = {};
     var handler = new DomHandler(null, options);
     new Parser(handler, options).end(detailHtml);
     DomUtils.getElementsByTagName('meta', handler.dom, true).forEach(function(el) {
-      if (!DomUtils.hasAttrib(el, 'itemprop')) {
+      var itempropValue = DomUtils.getAttributeValue(el, 'itemprop');
+      if (!itempropValue) {
         return;
       }
       // Split content like <meta itemprop="interactionCount" content="UserDownloads:418" />
-      if (DomUtils.getAttributeValue(el, 'itemprop') === 'interactionCount' &&
+      if (itempropValue === 'interactionCount' &&
         DomUtils.getAttributeValue(el, 'content').indexOf(':') !== -1) {
         var keyValue = DomUtils.getAttributeValue(el, 'content').split(':', 2);
-        elementItemProp = DomUtils.getAttributeValue(el, 'itemprop');
-        itemProps[elementItemProp] = itemProps[elementItemProp] || {};
+        itemProps[itempropValue] = itemProps[itempropValue] || {};
         if (includes(keysStringToFloat, keyValue[0])) {
-          itemProps[elementItemProp][keyValue[0]] = parseFloatWithComma(keyValue[1]);
+          itemProps[itempropValue][keyValue[0]] = parseFloatWithComma(keyValue[1]);
         } else {
-          itemProps[elementItemProp][keyValue[0]] = keyValue[1];
+          itemProps[itempropValue][keyValue[0]] = keyValue[1];
         }
       } else {
-        elementItemProp = DomUtils.getAttributeValue(el, 'itemprop');
-        if (includes(keysStringToFloat, elementItemProp)) {
-          itemProps[elementItemProp] = parseFloatWithComma(DomUtils.getAttributeValue(el, 'content'));
+        if (includes(keysStringToFloat, itempropValue)) {
+          itemProps[itempropValue] = parseFloatWithComma(DomUtils.getAttributeValue(el, 'content'));
         } else {
-          itemProps[elementItemProp] = DomUtils.getAttributeValue(el, 'content');
+          itemProps[itempropValue] = DomUtils.getAttributeValue(el, 'content');
         }
       }
     });
